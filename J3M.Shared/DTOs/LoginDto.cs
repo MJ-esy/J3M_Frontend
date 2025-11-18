99282e0c073a171c495a0d77e1e359aef943b983
jyneto <j-pm@live.se>
2025-11-18 17:36:40 +0100

    Create connection to DTO shared project from backend

---
+++ J3M.Shared/DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3M.Shared.DTOs
+{
+    internal class LoginDto
+    {
+    }
+}
