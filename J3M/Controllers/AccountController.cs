5a5925eef2ab1d8e78b1f3ce856316b2e757b332
jyneto <j-pm@live.se>
2025-11-19 10:41:26 +0100

    Created AccountController for login, logout purposes

---
+++ J3M/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace J3M.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+  
